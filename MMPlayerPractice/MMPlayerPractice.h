89b8eeeedc3a73e782c89b22747ce49d845a4818
Rameez Raja <rameez.cloud9@nxvt.com>
2021-01-18 17:16:38 +0500

    First commit

---
+++ MMPlayerPractice/MMPlayerPractice.h
@@ -0,0 +1,18 @@
+//
+//  MMPlayerPractice.h
+//  MMPlayerPractice
+//
+//  Created by Rameez Raja on 18/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MMPlayerPractice.
+FOUNDATION_EXPORT double MMPlayerPracticeVersionNumber;
+
+//! Project version string for MMPlayerPractice.
+FOUNDATION_EXPORT const unsigned char MMPlayerPracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMPlayerPractice/PublicHeader.h>
+
+
